3fb479d1757ecfcc0e6ed69d359b5b17a2a9c350
no-decode <127511474+no-decode@users.noreply.github.com>
2023-06-25 19:29:56 +0530

    Wrap decorator-2

--- 9-Decorators/1-Wrap-A-Function.py
+++ 9-Decorators/1-Wrap-A-Function.py
@@ -1,20 +1,17 @@
 import time
-from functools  import wraps
+from functools import wraps
 def timethis(func):
-    """ Decorator that prints execution duration """
     @wraps(func)
-    def wrapper(*args, **kwargs):
+    def wrapper(*args,**kwargs):
         start=time.time()
         result=func(*args,**kwargs)
         end=time.time()
         print(func.__name__,end-start)
         return result
     return wrapper
-
 @timethis
 def countdown(n):
-    while n > 0:
-        n -= 1
+    while(n>0):
+        n-=1
 
-""" countdown(10000)   """
 countdown(100000000)
