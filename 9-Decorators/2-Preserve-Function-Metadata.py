f6f3856995c4d8e8f5a659eb1382fe1be5d434cc
no-decode <127511474+no-decode@users.noreply.github.com>
2023-07-05 09:11:58 +0530

    Wrapped metadata

--- 9-Decorators/2-Preserve-Function-Metadata.py
+++ 9-Decorators/2-Preserve-Function-Metadata.py
@@ -4,16 +4,17 @@ def timethis(func):
     @wraps(func)
     def wrapper(*args,**kwargs):
         start=time.time()
-        # result=func(*args,**kwargs)
-        func(*args,**kwargs)
+        result=func(*args,**kwargs)
         end=time.time()
         print(func.__name__,end-start)
-        # return result
+        return result
     return wrapper
 @timethis
-def countdown(n):
-    while (n>0):
+def countdown(n:int):
+    while n > 0:
         n-=1
-# clear
-countdown(5000000)
+# countdown(1000000)
 countdown.__name__
+
+
+    
