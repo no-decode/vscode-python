327ef194d17af84a166f767dceaa03fc45ff210d
no-decode <127511474+no-decode@users.noreply.github.com>
2023-07-21 15:16:25 +0530

    R functions

--- 9-Decorators/4-Decorator-With-Arguments.py
+++ 9-Decorators/4-Decorator-With-Arguments.py
@@ -1,2 +1,32 @@
-import logging
 from functools import wraps
+import logging
+def logged(level, name=None, message=None):
+    '''
+    Add logging to a function. level is the logging
+    level, name is the logger name, and message is the
+    log message. If name and message aren't specified,
+    they default to the function's module and name.
+    '''
+    def decorate(func):
+            logname = name if name else func.__module__
+            log = logging.getLogger(logname)
+            logmsg = message if message else func.__name__
+            @wraps(func)
+            def wrapper(*args, **kwargs):
+                log.log(level, logmsg)
+                return func(*args, **kwargs)
+            return wrapper
+    return decorate
+# Example use
+@logged(logging.CRITICAL)
+def add(x, y):
+    return x + y
+@logged(logging.CRITICAL, 'example')
+def spam():
+ print('Spam!')
+@logged(logging.CRITICAL)
+def func(a,b):
+    pass
+add(2,3)
+spam()
+func(1,2)
